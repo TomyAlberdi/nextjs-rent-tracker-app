08d6f20f959e4aaca18cff499b409323a424e976
Tomás Alberdi <tomas.alberdi.2001@gmail.com>
2025-10-29 18:18:52 -0300

    implemented property context

---
+++ src/app/layout.tsx
@@ -0,0 +1,45 @@
+"use client";
+import "@/app/globals.css";
+import { AppSidebar } from "@/components/app-sidebar";
+import { Separator } from "@/components/ui/separator";
+import {
+  SidebarInset,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar";
+import PropertyContextComponent from "@/context/property/PropertyContextComponent";
+import { Toaster } from "sonner";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <PropertyContextComponent>
+          <SidebarProvider>
+            <AppSidebar />
+            <SidebarInset>
+              <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                <div className="flex items-center gap-2 px-4">
+                  <SidebarTrigger className="-ml-1" />
+                  <Separator
+                    orientation="vertical"
+                    className="mr-2 data-[orientation=vertical]:h-4"
+                  />
+                  {/* 
+                  //TODO: build nextjs breadcrumb
+                */}
+                </div>
+              </header>
+              {children}
+            </SidebarInset>
+          </SidebarProvider>
+          <Toaster />
+        </PropertyContextComponent>
+      </body>
+    </html>
+  );
+}
---
+++ src/context/property/PropertyContext.tsx
@@ -0,0 +1,15 @@
+import { CreatePropertyDTO, Property } from "@/lib/interfaces";
+import { createContext } from "react";
+
+export interface PropertyContextType {
+  getProperties: () => Promise<Property[]>;
+  getPropertyById: (id: string) => Promise<Property | null>;
+  createProperty: (property: CreatePropertyDTO) => Promise<void>;
+  updateProperty: (
+    groupId: string,
+    property: CreatePropertyDTO
+  ) => Promise<void>;
+  deleteProperty: (id: string) => Promise<void>;
+}
+
+export const PropertyContext = createContext<PropertyContextType | null>(null);
